a868536b333547a60b67f7909763c9252003fe6b
yunus bozkurtaca <yunus21bzkrtc@gmail.com>
2025-06-25 17:31:23 +0300

    Add StrategyItemDelegate and update StrategiesListView
    Introduces the StrategyItemDelegate header for custom item rendering in StrategiesListView and registers it in the CMakeLists.txt. Also updates StrategiesListView in mainwindow.ui to support extended selection mode.

---
+++ Views/StrategyItemDelegate.h
@@ -0,0 +1,20 @@
+#ifndef STRATEGYITEMDELEGATE_H
+#define STRATEGYITEMDELEGATE_H
+
+#include <QStyledItemDelegate>
+#include <QObject>
+#include <QPainter>
+
+class StrategyDelegate : public QStyledItemDelegate{
+    Q_OBJECT
+
+public:
+    StrategyDelegate(QObject *parent = nullptr);
+
+    void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
+
+    // Satır yüksekliğini ayarla:
+    QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const override;
+};
+
+#endif // STRATEGYITEMDELEGATE_H
